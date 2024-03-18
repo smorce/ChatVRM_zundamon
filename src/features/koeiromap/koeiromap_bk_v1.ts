4b3f1badb7a298946e34272e250435f516baa79b
smorce <smorcepie@gmail.com>
2024-03-18 12:30:41 +0900

    修正

--- src/features/koeiromap/koeiromap_bk_v1.ts
+++ src/features/koeiromap/koeiromap_bk_v1.ts
@@ -24,24 +24,17 @@ export async function style_bert_vits2(
   reference_audio_path: string | null = null,
   given_tone: boolean = false
 ): Promise<string> { // Base64エンコードされた文字列を返す
-
-  // URLを構築し、クエリパラメータ`text`を追加
-  // ★URL は毎回書き換える
-  const PUBLIC_URL = "https://f431-34-90-206-27.ngrok-free.app";
-  const url = new URL(`${PUBLIC_URL}/voice`);
-  url.searchParams.append('text', message); // `message`をクエリパラメータに追加
-
   const param = {
     method: "POST",
     body: JSON.stringify({
-      // text: message,                            // 変換するテキスト（エラーが出るので、ここでは入れずにURLに含めるようにした）
+      text: message,                            // 変換するテキスト
       speaker_id: speaker_id,                    // 話者のID
       sdp_ratio: sdp_ratio,                      // SDP（Stochastic Duration Predictor）とDP（Duration Predictor）の混合比率
       noise: noise,                              // サンプルノイズの割合（ランダム性を増加させる）
       noisew: noisew,                            // SDPノイズの割合（発音の間隔のばらつきを増加させる）
       length: length,                            // 話速（1が標準）
       language: language,                        // テキストの言語
-      auto_split: auto_split.toString(),         // booleanを文字列に変換。自動でテキストを分割するか
+      auto_split: auto_split.toString(),         //  booleanを文字列に変換。自動でテキストを分割するか
       split_interval: split_interval,            // 分割した際の無音区間の長さ（秒）
       assist_text: assist_text,                  // 補助テキスト（読み上げと似た声音・感情になりやすい）
       assist_text_weight: assist_text_weight,    // 補助テキストの影響の強さ
@@ -56,12 +49,15 @@ export async function style_bert_vits2(
     },
   };
 
-  // `fetch`を使用してAPIリクエストを送信、URLにクエリパラメータを含める
-  const response = await fetch(url.toString(), param);
+  // ★URL は毎回書き換える
+  const PUBLIC_URL = "https://f431-34-90-206-27.ngrok-free.app";
+  // fetchを使用してAPIリクエストを送信
+  const response = await fetch(`${PUBLIC_URL}/voice`, param);
 
   if (!response.ok) {
     throw new Error(`Request failed: ${response.status}`);
   }
+
 
   // レスポンスの内容（Blob）を取得
   const blob = await response.blob();
